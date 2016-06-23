8ed8b42af31916f4fa93db504b0470f690f466da
sprt <hellosprt@gmail.com>
2016-06-23 12:13:39 +0200

    Make LsTreeEntry.Filename relative

--- cmd/git-gobreaking/git/git.go
+++ cmd/git-gobreaking/git/git.go
@@ -2,7 +2,6 @@ package git
 
 import (
 	"os/exec"
-	"path/filepath"
 	"strings"
 )
 
@@ -35,20 +34,13 @@ func LsTree(recursive bool, treeish string, path string) ([]*LsTreeEntry, error)
 	var entries []*LsTreeEntry
 	for _, line := range strings.Split(string(out)[:len(out)-1], "\n") {
 		fields := strings.Fields(line)
-
 		var kind ObjectKind
 		if fields[1] == "tree" {
 			kind = Tree
 		} else {
 			kind = Blob
 		}
-
-		abs, err := filepath.Abs(fields[3])
-		if err != nil {
-			return nil, err
-		}
-
-		entries = append(entries, &LsTreeEntry{Filename: abs, Kind: kind})
+		entries = append(entries, &LsTreeEntry{Filename: fields[3], Kind: kind})
 	}
 
 	return entries, nil
